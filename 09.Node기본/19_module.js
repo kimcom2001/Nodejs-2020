27a53007e8583d33ed6143de5842e1acbd00b8b4
Youngjoong Kim <kimcom2001@naver.com>
2020-09-28 16:29:10 +0900

    Node JS 서버 적용

--- 09.Node기본/19_module.js
+++ 09.Node기본/19_module.js
@@ -1,5 +1,9 @@
-const myModule = require(`./20_myModule.js`);
+module.exports = {
+    randInt: function(from, to) {
+        return Math.floor(Math.random()*(to-from+1) + from);
+    },
 
-for (let i=0; i<5; i++) {
-    console.log(myModule.area(myModule.randInt(1,5)));
+    area: function(radius) {
+        return Math.PI * radius * radius;
+    }
 }
--- 09.Node기본/19_random.js
+++ 09.Node기본/19_random.js
@@ -1,4 +1,4 @@
-const myModule = require('./18_exports.js');
+const myModule = require('./19_module.js');
 
 for (let i=0; i<5; i++) {
     console.log(myModule.area(myModule.randInt(1, 5)));
